be0203a10b38af7e38e2c1e4daa433597365e61d
Mike Decker <mdecker@michaeldecker.vermillion.eaglecrk.local>
2014-09-04 09:54:06 -0500

    feature basic content update

--- sites/all/modules/features/basic_setup/feature_basic_content/draggable_views.js
+++ sites/all/modules/features/basic_setup/feature_basic_content/draggable_views.js
@@ -1,9 +1,10 @@
+/*
 (function ($) {
     Drupal.behaviors.test = {
         attach: function (context, settings) {
             $(document).ready(function () {
                 //Move the quicktabs into the contextual links gear and hides the actual tabs from view.
-                $("[id^=block-quicktabs-draggable-views-]").each(function () {
+                $("[id^=block-quicktabs-dragview-]").each(function () {
                     var links = $(this).find('ul.quicktabs-tabs').contents();
                     $(this).find('.contextual-links-wrapper:first ul').append(links);
                     $(this).find('.contextual-links-wrapper:first').css('right', '50px');
@@ -14,3 +15,14 @@
         }
     }
 })(jQuery);
+*/
+
+jQuery(document).ready(function ($) {
+    $("[id^=block-quicktabs-dragview-]").each(function () {
+        var links = $(this).find('ul.quicktabs-tabs').contents();
+        $(this).find('.contextual-links-wrapper:first ul').append(links);
+        $(this).find('.contextual-links-wrapper:first').css('right', '50px');
+        $(this).find('h2.block-title:first').remove();
+        $(this).find('ul.quicktabs-tabs').remove();
+    });
+});
