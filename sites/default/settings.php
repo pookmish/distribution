e28f6dc84c67938c55af016bce1e2fefc49ca14f
Mike Decker <mdecker@michaeldecker.vermillion.eaglecrk.local>
2014-09-10 14:08:34 -0500

    modified feature_basic_event, included a calendar view

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -210,7 +210,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'ec-distro',
+      'username' => 'root',
+      'password' => '',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -242,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'ezCjvQ79KgfIC8SWmmZGvjpeINt4YxeBCLYu5LsaJJA';
 
 /**
  * Base URL (optional).
