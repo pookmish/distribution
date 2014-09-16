b4ffe6dc0e5181f37f7b1f7e6e265302b0d42aae
Mike Decker <mdecker@michaeldecker.vermillion.eaglecrk.local>
2014-09-16 14:36:54 -0500

    updated file_entity to dev version. added support for youtube in media module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -256,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'rRxO-iwhrHza3mK5HwuhK4vextzpzsWLnGPbSQ_hWkQ';
+$drupal_hash_salt = 'xDybZ580-FaSYQDNL9pejQ8L2JQVAM49g2ynlyBZsI8';
 
 /**
  * Base URL (optional).
