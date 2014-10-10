4475c1eb400808dd02ef23aad3121b5dacd51078
Paul Kloos <pkloos@eaglecrk.com>
2014-10-10 09:37:14 -0500

    added function documentation

--- sites/all/themes/bootstrap/bootstrap_subtheme/template.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/template.php
@@ -1,14 +1,24 @@
 <?php
+/**
+ * @file
+ * template.php
+ */
+ /**
+ * Override or insert variables into the page template.
+ */
+function bootstrap_lps_process_page(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_page_alter($variables);
+  }
+}
+ /**
+ * Override or insert variables into the page template for HTML output.
+ */
+function bootstrap_lps_process_html(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_html_alter($variables);
+  }
+}
 
-function bootstrap_subtheme_process_html(&$vars){
-  //color module support
-  if (module_exists('color')) {
-    _color_html_alter($vars);
-  }
-}
-function bootstrap_subtheme_process_page(&$vars){
-  //color module support
-  if (module_exists('color')) {
-    _color_page_alter($vars);
-  }
-}
